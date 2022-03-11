d81de4ac386e3853bbd2d45ab00ec30341ec2b39
akhmadamanulloh <akhmad.amanulloh11@gmail.com>
2022-03-11 17:04:25 +0700

    menambah file hello.php

---
+++ hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html> 
+<head>
+    <title>Nomer 1</title>
+</head>
+<body>
+    <?php
+        echo "<h1>Hello</h1>"; //ini adalah file php
+        echo "Nama Saya Akhmad Amanulloh<br>";
+        echo "NIM 200411100099";
+    ?>
+</body>
+</html> 
